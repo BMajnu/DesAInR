98125234a1534c9e88cda10452f3c5e6634beb7c
Badiuzzaman Majnu <badiuzzaman.majnu@gmail.com>
2025-05-19 19:51:24 +0600

    Add vercel.json and next.config.js for proper deployment

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // Adding allowed origins for browser preview
+  allowedDevOrigins: ['127.0.0.1'],
+};
+
+module.exports = nextConfig;
